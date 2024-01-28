58aba476b9cb13fd1d7b8fad804066592d634751
Nishimara <admin@abdulakh.fun>
2024-01-28 19:14:47 +0300

    added config for firefox

--- flake.nix
+++ flake.nix
@@ -16,31 +16,36 @@
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixvim, hyprland, hyprland-plugins }@inputs: 
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      nixvimModule = nixvim.homeManagerModules.nixvim;
-    in {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-        modules = [
-          hyprland.nixosModules.default {
-            programs.hyprland.enable = true; #needed in system and in user so the hyprland always up-to-date
-          }
-	        ./system
-	      ];
-      };
-      defaultPackage.${system} = home-manager.defaultPackage.${system};
+  outputs = { self, nixpkgs, home-manager, nixvim, hyprland, hyprland-plugins, firefox-addons }@inputs: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    nixvimModule = nixvim.homeManagerModules.nixvim;
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        hyprland.nixosModules.default {
+          programs.hyprland.enable = true; #needed in system and in user so the hyprland always up-to-date
+        }
+        ./system
+      ];
+    };
+    defaultPackage.${system} = home-manager.defaultPackage.${system};
 
-      homeConfigurations."ayako" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-	      modules = [
-	        ./home
-          nixvimModule
-        ];
-        extraSpecialArgs = { inherit inputs; };
-      };
+    homeConfigurations."ayako" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        nixvimModule
+        ./home
+      ];
+      extraSpecialArgs = { inherit inputs; };
+    };
   };
 }
