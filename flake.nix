0635c0ffd87a88ae2c8994e6f5f34e2c177ef11c
Nishimara <me@nishimara.com>
2024-04-21 19:52:50 +0300

    flake: use builtins fetchTree instead of not-yet initializated nixpkgs

--- flake.nix
+++ flake.nix
@@ -30,11 +30,12 @@
 
     # pin telegram to v4.15.2
     pkgs = nixpkgs.legacyPackages.${system}.extend (final: prev: {
-      telegram-desktop = (import (prev.fetchTree {
+      telegram-desktop = (import (builtins.fetchTree {
+        type = "github";
         owner = "NixOS";
         repo = "nixpkgs";
         rev = "3f178e415639bd509b2cb09f9e56b7994f11ed17";
-        hash = "sha256-1Q0Tk3hBHfZ7RxK1YtcsUMfdUWLhqYCsy6bP1piEVOs=";
+        narHash = "sha256-1Q0Tk3hBHfZ7RxK1YtcsUMfdUWLhqYCsy6bP1piEVOs=";
       }) { inherit system; }).pkgs.telegram-desktop;
     });
   in {
