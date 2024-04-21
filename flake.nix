35180a72023cdfdf1a377b9f7f08e5770c64b4b0
Nishimara <me@nishimara.com>
2024-04-21 03:15:00 +0300

    flake: pin telegram-desktop to 4.15.2

--- flake.nix
+++ flake.nix
@@ -27,26 +27,41 @@
   outputs = { self, nixpkgs, home-manager, agenix, nixvim, hyprland, firefox-addons }@inputs: 
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+
+    # pin telegram to v4.15.2
+    pkgs = nixpkgs.legacyPackages.${system}.extend (final: prev: {
+      telegram-desktop = (import (prev.fetchTree {
+        owner = "NixOS";
+        repo = "nixpkgs";
+        rev = "3f178e415639bd509b2cb09f9e56b7994f11ed17";
+        hash = "sha256-1Q0Tk3hBHfZ7RxK1YtcsUMfdUWLhqYCsy6bP1piEVOs=";
+      }) { inherit system; }).pkgs.telegram-desktop;
+    });
   in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       inherit system;
+
       modules = [
         ./system
+
         hyprland.nixosModules.default {
-          programs.hyprland.enable = true; #needed in system and in user so the hyprland always up-to-date
+          programs.hyprland.enable = true; #needed in system and in user configurations
         }
+
         agenix.nixosModules.default
       ];
+
       specialArgs = { inherit inputs; };
     };
 
     homeConfigurations."ayako" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
+
       modules = [
-        nixvim.homeManagerModules.nixvim
         ./home
+        nixvim.homeManagerModules.nixvim
       ];
+
       extraSpecialArgs = { inherit inputs; };
     };
   };
