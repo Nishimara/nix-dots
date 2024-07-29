d8c2d4b12d491797c6266f8998cee20c2912d1bd
Nishimara <me@nishimara.com>
2024-07-30 00:54:41 +0300

    secrets: add key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGHNm+K2k+ElPIKtjRuMt1tvkjaDy310wbTiWIVNDwK";
-  systems = [ nixos ];
+  thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILn48m44TCPXnnjk8DBG24lRoAuL/3AckfkkrO9yFCJX";
+  systems = [ nixos thinkpad ];
 in {
   "xray.age".publicKeys = systems;
 }
