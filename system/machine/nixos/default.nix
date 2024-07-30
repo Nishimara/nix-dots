01eb193f97fed6b31b0311245590504980a8770e
Nishimara <me@nishimara.com>
2024-07-30 20:37:23 +0300

    remove tmpfs on /tmp

--- system/machine/nixos/default.nix
+++ system/machine/nixos/default.nix
@@ -17,10 +17,7 @@
       };
       efi.canTouchEfiVariables = true;
     };
-    tmp = {
-      useTmpfs = true;
-      cleanOnBoot = true;
-    };
+    tmp.cleanOnBoot = true;
     kernelPackages = pkgs.linuxPackages_zen;
   };
 
@@ -89,7 +86,18 @@
   time.timeZone = "Europe/Moscow";
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    supportedLocales = [ "en_US.UTF-8/UTF-8" "ru_RU.UTF-8/UTF-8" ];
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
 
   console = {
