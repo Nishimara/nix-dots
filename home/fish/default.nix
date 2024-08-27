64360d2bf79de24476f43c166cef89ecbdba7066
Nishimara <me@nishimara.com>
2024-08-27 20:45:48 +0300

    fish: add dracula theme

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -4,7 +4,8 @@
     enable = true;
 
     interactiveShellInit = ''
-      set fish_greeting; # Disable greeting
+      set fish_greeting;
+      fish_config theme choose "Dracula";
 
       ${pkgs.nix-your-shell}/bin/nix-your-shell fish | source
 
@@ -19,4 +20,13 @@
       { name = "fzf"; src = pkgs.fishPlugins.fzf.src; }
     ];
   };
+
+  xdg.configFile."fish/themes/Dracula.theme".source = let
+    dracula = pkgs.fetchFromGitHub {
+      owner = "dracula";
+      repo = "fish";
+      rev = "269cd7d76d5104fdc2721db7b8848f6224bdf554";
+      sha256 = "sha256-Hyq4EfSmWmxwCYhp3O8agr7VWFAflcUe8BUKh50fNfY=";
+    };
+  in "${dracula}/themes/Dracula Official.theme";
 }
