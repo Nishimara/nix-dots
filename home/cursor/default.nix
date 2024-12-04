22607551e810992973c390dfa31bf741c8a8f376
Nishimara <me@nishimara.com>
2024-12-05 01:24:50 +0300

    home: set cursor size to 24

--- home/cursor/default.nix
+++ home/cursor/default.nix
@@ -3,14 +3,14 @@
 {
   wayland.windowManager.hyprland.settings.env = [
     "HYPRCURSOR_THEME,catppuccin-mocha-mauve-cursors"
-    "HYPRCURSOR_SIZE,18"
+    "HYPRCURSOR_SIZE,24"
   ];
 
   # xcursor
   home.pointerCursor = {
     name = "catppuccin-mocha-mauve-cursors";
     package = pkgs.catppuccin-cursors.mochaMauve;
-    size = 18;
+    size = 24;
 
     gtk.enable = true;
     x11.enable = true;
