a09952d81cc49e2d1f67d72831bb852960ada87e
Nishimara <me@nishimara.com>
2024-12-06 03:06:36 +0300

    hyprland: remove config warnings

--- home/wm/hyprland/default.nix
+++ home/wm/hyprland/default.nix
@@ -79,10 +79,6 @@ in
       decoration = {
         rounding = 10;
         blur = {};
-        drop_shadow = "yes";
-        shadow_range = 4;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(1a1a1aee)";
       };
 
       animations = {
