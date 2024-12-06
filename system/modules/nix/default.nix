b6355bf282a6fe13f0e71c980a83f6c6b410160a
Nishimara <me@nishimara.com>
2024-12-06 03:11:14 +0300

    nix: alias `n` to `nixpkgs`

--- system/modules/nix/default.nix
+++ system/modules/nix/default.nix
@@ -5,7 +5,7 @@
     nix = {
       channel.enable = false;
 
-      registry = lib.mapAttrs (_: flake: { inherit flake; }) inputs;
+      registry = (lib.mapAttrs (_: flake: { inherit flake; }) inputs) // { n.flake = inputs.nixpkgs; };
 
       settings = {
         experimental-features = [ "nix-command" "flakes" ];
@@ -13,11 +13,13 @@
 
         substituters = [
           "https://hyprland.cachix.org"
+          "https://nishimara.cachix.org"
           "https://nix-gaming.cachix.org"
         ];
 
         trusted-public-keys = [
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nishimara.cachix.org-1:QC+NoP8D4tsIicaGBXRwQVn9Pl26q77AFVbPkZKEmis="
           "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         ];
       };
