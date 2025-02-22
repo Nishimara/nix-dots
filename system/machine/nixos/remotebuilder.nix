e615d4a591c3de61a3fa1a578f372ed5dfd32f97
Nishimara <me@nishimara.com>
2025-02-22 14:26:41 +0300

    optimise remote builder

--- system/machine/nixos/remotebuilder.nix
+++ system/machine/nixos/remotebuilder.nix
@@ -11,5 +11,21 @@
     groups.remotebuilder = {};
   };
 
-  nix.settings.trusted-users = [ "remotebuilder" ];
+  nix = {
+    nrBuildUsers = 64;
+    settings = {
+      trusted-users = [ "remotebuilder" ];
+
+      min-free = 10 * 1024 * 1024;
+
+      max-jobs = "auto";
+      cores = 0;
+    };
+  };
+
+  systemd.services.nix-daemon.serviceConfig = {
+    MemoryAccounting = true;
+    MemoryMax = "80%";
+    OOMScoreAdjust = 500;
+  };
 }
