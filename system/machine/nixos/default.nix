b3ca2afd31d149766ebc88abe55d5adf936c4bfa
nishimara <me@nishimara.com>
2025-02-23 20:15:47 +0300

    system.stateVersion: 24.05 -> 24.11

--- system/machine/nixos/default.nix
+++ system/machine/nixos/default.nix
@@ -249,5 +249,5 @@
     ];
   };
 
-  system.stateVersion = "24.05";
+  system.stateVersion = "24.11";
 }
--- system/machine/thinkpad/default.nix
+++ system/machine/thinkpad/default.nix
@@ -176,5 +176,5 @@
     ];
   };
 
-  system.stateVersion = "24.05";
+  system.stateVersion = "24.11";
 }
