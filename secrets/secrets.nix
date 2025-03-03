15c70ba266a98c2b927ad7f88af1216672219550
nishimara <me@nishimara.com>
2025-03-03 19:29:19 +0300

    secrets: re-key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGHNm+K2k+ElPIKtjRuMt1tvkjaDy310wbTiWIVNDwK";
+  nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYj9Dd4kG2oshwinQ0wrBkMv7YU5z2qbB8ct65th5Ul";
   thinkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILn48m44TCPXnnjk8DBG24lRoAuL/3AckfkkrO9yFCJX";
   systems = [ nixos thinkpad ];
 in {
