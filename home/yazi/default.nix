e03dee0d1a19f0ac4362e5b03ea0b5b2a178e1bc
Nishimara <me@nishimara.com>
2025-03-13 01:14:53 +0300

    yazi: add configuration

--- home/yazi/default.nix
+++ home/yazi/default.nix
@@ -1,5 +1,29 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.yazi = {
     enable = true;
+
+    settings = {
+      manager = {
+        sort_by = "natural";
+        sort_sensitive = true;
+        sort_reverse = false;
+        sort_dir_first = true;
+
+        show_hidden = true;
+        show_symlink = true;
+
+        linemode = "none";
+      };
+
+      preview = {
+        tab_size = 2;
+      };
+
+      opener = {
+        play = [
+          { run = "${pkgs.vlc}/bin/vlc $@"; orphan = true; }
+        ];
+      };
+    };
   };
 }
