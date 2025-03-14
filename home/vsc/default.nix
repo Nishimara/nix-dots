6d389e24eb71f92f5eaf20f823c674ca05607fbd
Nishimara <me@nishimara.com>
2025-03-14 15:50:07 +0300

    flake: update lock

--- home/vsc/default.nix
+++ home/vsc/default.nix
@@ -4,20 +4,22 @@
     enable = true;
     package = pkgs.vscodium;
 
-    extensions = with pkgs.vscode-extensions; [
-      dracula-theme.theme-dracula
-      pkief.material-icon-theme
-    ];
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+        pkief.material-icon-theme
+      ];
 
-    userSettings = {
-      "workbench.colorTheme" = "Dracula";
-      "workbench.iconTheme" = "material-icon-theme";
-      "editor.fontSize" = 16;
-      "editor.fontFamily" = "Hack Nerd Font";
-      "terminal.integrated.fontFamily" = "monospace";
-      "editor.unicodeHighlight.allowedLocales".ru = true;
-      "window.titleBarStyle" = "custom";
-      "editor.renderLineHighlight" = "none";
+      userSettings = {
+        "workbench.colorTheme" = "Dracula";
+        "workbench.iconTheme" = "material-icon-theme";
+        "editor.fontSize" = 16;
+        "editor.fontFamily" = "Hack Nerd Font";
+        "terminal.integrated.fontFamily" = "monospace";
+        "editor.unicodeHighlight.allowedLocales".ru = true;
+        "window.titleBarStyle" = "custom";
+        "editor.renderLineHighlight" = "none";
+      };
     };
   };
 }
